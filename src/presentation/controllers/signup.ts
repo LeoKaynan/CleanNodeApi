04e6ed164ad3ea793df37298446a51d56cc1c72b
Leonardo Kaynan <leokaynan@hotmail.com>
2022-05-14 23:41:54 -0300

    feat: ensure SignUpController returns an error if any field is not provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,7 +3,14 @@ export class SignUpController {
     const requiredFields =
       ['name', 'email', 'password', 'passwordConfirmation'];
     for (const field of requiredFields) {
-      if (!request.body[field]) return {statusCode: 400};
+      if (!request.body[field]) {
+        return (
+          {
+            statusCode: 400,
+            body: new Error('these fields are required: name, email, password and password confirmation'),
+          }
+        );
+      }
     }
     return {statusCode: 200};
   };
