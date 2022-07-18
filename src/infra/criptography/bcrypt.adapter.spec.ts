c5186dc3001092c46ca7718136d740d78b59d133
Leonardo Kaynan <leokaynan@hotmail.com>
2022-07-17 22:30:31 -0300

    test: ensure BcryptAdapter returns hashed value

--- src/infra/criptography/bcrypt.adapter.spec.ts
+++ src/infra/criptography/bcrypt.adapter.spec.ts
@@ -1,11 +1,23 @@
 import bcrypt from 'bcrypt';
 import {BcryptAdapter} from './bcrypt.adapter';
 
+jest.mock('bcrypt', () => ({
+  async hash(): Promise<string> {
+    return Promise.resolve('hash');
+  },
+}));
+
 describe('Bcrypt Adapter', () => {
   const sut = new BcryptAdapter();
+  const salt = 12;
+
   test('Should call bcrypt with correct values', async () => {
     const hashSpy = jest.spyOn(bcrypt, 'hash');
     await sut.encrypt('valid_password');
-    expect(hashSpy).toHaveBeenCalledWith('valid_password', 12);
+    expect(hashSpy).toHaveBeenCalledWith('valid_password', salt);
+  });
+  test('Should BcryptAdapter returns hashed value', async () => {
+    const result = await sut.encrypt('valid_password');
+    expect(result).toBe('hash');
   });
 });
