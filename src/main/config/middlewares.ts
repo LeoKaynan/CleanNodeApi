4cdc26521d4d3456ec3be51dccd8ecbe1fb5961b
Leonardo Kaynan <leokaynan@hotmail.com>
2022-07-24 19:16:15 -0300

    feat: ensure cos enabled

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,14 +1,24 @@
-import {Express, json} from 'express';
+import {Express, json, Request, Response, NextFunction} from 'express';
 
 export class Middlewares {
   constructor(private app: Express) {}
 
   init() {
     this.bodyParser();
+    this.cors();
   }
 
   bodyParser() {
     this.app.use(json());
+  }
+
+  cors() {
+    this.app.use((_req: Request, res: Response, next: NextFunction) => {
+      res.setHeader('access-control-allow-origin', '*');
+      res.setHeader('access-control-allow-headers', '*');
+      res.setHeader('access-control-allow-methods', '*');
+      next();
+    });
   }
 }
 
