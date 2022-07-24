d141f6c7c3093ddf77dc2b0cb62e142d9b36b2cf
Leonardo Kaynan <leokaynan@hotmail.com>
2022-07-24 19:16:30 -0300

    test: ensure cos enabled

--- src/main/config/middlewares.test.ts
+++ src/main/config/middlewares.test.ts
@@ -9,4 +9,13 @@ describe('Middlewares', () => {
         .send({test: 'testing'})
         .expect({test: 'testing'});
   });
+
+  test('Should enable cors', async () => {
+    app.get('/test_cors', (req, res) => res.send());
+    await request(app)
+        .get('/test_cors')
+        .expect('access-control-allow-origin', '*')
+        .expect('access-control-allow-headers', '*')
+        .expect('access-control-allow-methods', '*');
+  });
 });
